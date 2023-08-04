6345091889eb94a86c12bb7349cd8a3ae71e8209
rominarg <rominagnavarrete@gmail.com>
2023-08-04 17:08:49 -0300

    second commit

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,8 +1,11 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -11,9 +14,10 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 
-	public CommandLineRunner initData(){
+	@Bean
+	public CommandLineRunner initData(ClientRepository repository){
 		return (args ->{
-
+			repository.save(new Client("Melba", "Morel","melba@mindhub.com"));
 		});
 	}
 }
